125fe155b91c3539d1703cea4e2bb0242dc9c897
ishubin0309 <ivan.shubin0309@gmail.com>
2021-03-10 21:59:56 +0100

    update

---
+++ src/AppBundle/Service/AnnuaireMailer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Message;
+
+/**
+ * Haffoudhi
+ */
+class AnnuaireMailer
+{
+    private $api_key;
+
+    public function __construct($api_key)
+    {
+        $this->api_key = $api_key;
+    }
+    
+    public function handleMessage(Message $message, &$errors)
+    {
+        $email = new \SendGrid\Mail\Mail();
+        $email->setFrom('climactif@hotmail.com', 'Climactif');
+        $email->setSubject($message->getObject());
+        $email->addTo($message->getTo());
+        $email->addContent("text/plain", strip_tags(str_replace("\n", '<br>', $message->getBody())));
+        $email->addContent(
+            "text/html", $message->getBody()
+        );
+        $sendgrid = new \SendGrid($this->api_key);
+        try {
+            $response = $sendgrid->send($email);
+            $wasSendingSuccessful = true;
+        } catch (Exception $e) {
+            $errors[] = $e->getMessage();
+            $wasSendingSuccessful = false;
+        }
+
+        if ($wasSendingSuccessful) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
