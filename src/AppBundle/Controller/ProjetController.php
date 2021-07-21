8d1e77d234fb8686de46fa1cb1536c69a1f5731d
ishubin0309 <ivan.shubin0309@gmail.com>
2021-07-21 17:27:34 +0100

    update

--- src/AppBundle/Controller/ProjetController.php
+++ src/AppBundle/Controller/ProjetController.php
@@ -823,8 +823,7 @@ class ProjetController extends Controller
 			for ($i=0; $i <= 4; $i++) {
 				$j = $i > 0 ? $i : '';
 				if (isset($_POST['{proprietaire' . $j . '_identite}']) && $_POST['{proprietaire' . $j . '_identite}']) {
-					echo '<pre>';print_r([$_POST['proprietaire' . $j . '_civilite}'], $_POST['proprietaire' . $j . '_identite}'], $_POST['proprietaire' . $j . '_ne_le}'], $_POST['proprietaire' . $j . '_ne_a}'], $_POST['proprietaire' . $j . '_adresse}'], $_POST['proprietaire' . $j . '_droit}'], $_POST['proprietaire' . $j . '_marital}'], $_POST['proprietaire' . $j . '_qualite}']]);
-					$personnesPhysiquesRow.= str_replace(['{proprietaire_civilite}', '{proprietaire_identite}', '{proprietaire_ne_le}', '{proprietaire_ne_a}', '{proprietaire_adresse}', '{proprietaire_droit}', '{proprietaire_marital}', '{proprietaire_qualite}'], [$_POST['proprietaire' . $j . '_civilite}'], $_POST['proprietaire' . $j . '_identite}'], $_POST['proprietaire' . $j . '_ne_le}'], $_POST['proprietaire' . $j . '_ne_a}'], $_POST['proprietaire' . $j . '_adresse}'], $_POST['proprietaire' . $j . '_droit}'], $_POST['proprietaire' . $j . '_marital}'], $_POST['proprietaire' . $j . '_qualite}']], $personnesPhysiquesRowCopy);
+					$personnesPhysiquesRow.= str_replace(['{proprietaire_civilite}', '{proprietaire_identite}', '{proprietaire_ne_le}', '{proprietaire_ne_a}', '{proprietaire_adresse}', '{proprietaire_droit}', '{proprietaire_marital}', '{proprietaire_qualite}'], [$_POST['{proprietaire' . $j . '_civilite}'], $_POST['{proprietaire' . $j . '_identite}'], $_POST['{proprietaire' . $j . '_ne_le}'], $_POST['{proprietaire' . $j . '_ne_a}'], $_POST['{proprietaire' . $j . '_adresse}'], $_POST['{proprietaire' . $j . '_droit}'], $_POST['{proprietaire' . $j . '_marital}'], $_POST['{proprietaire' . $j . '_qualite}']], $personnesPhysiquesRowCopy);
 				}
 			}exit($personnesPhysiquesRow);
 
