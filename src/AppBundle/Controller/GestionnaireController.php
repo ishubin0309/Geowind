66683b996430b734c46c8710dace1c7bfa044888
ishubin0309 <ivan.shubin0309@gmail.com>
2022-05-01 18:29:43 +0100

    add gestionnaire_update

--- src/AppBundle/Controller/GestionnaireController.php
+++ src/AppBundle/Controller/GestionnaireController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\User\UserInterface;
 
