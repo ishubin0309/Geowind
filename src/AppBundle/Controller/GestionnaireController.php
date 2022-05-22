4fc62c0d81b7145ebf1993ff1deb09de31bc200c
ishubin0309 <ivan.shubin0309@gmail.com>
2022-05-23 00:03:30 +0100

    update

--- src/AppBundle/Controller/GestionnaireController.php
+++ src/AppBundle/Controller/GestionnaireController.php
@@ -26,12 +26,22 @@ class GestionnaireController extends Controller
     public function gestionnaireAction(Request $request)
     {   
         $em = $this->getDoctrine()->getManager();
+        
+        $import = new Import();
+        $form = $this->createForm(ImportType::class, $import);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file_path = $import->getImportFile();
+            $this->importFile($file_path);
+        }
 
         $gestionnaires = $em->getRepository('AppBundle:Gestionnaire')
                         ->findAll();
 
         return $this->render('gestionnaire/gestionnaire.html.twig', [
             'gestionnaires' => $gestionnaires,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -46,63 +56,70 @@ class GestionnaireController extends Controller
         $file_path = __DIR__ . '/../Gestionnaires.csv';
         $row = 0;
         if(file_exists($file_path)) {
-            if (($handle = fopen($file_path, "r")) !== FALSE) {
-                ini_set("memory_limit", "4000M");
-                set_time_limit(3000);
-                ini_set('display_errors', 1);
-                ini_set('display_startup_errors', 1);
-                error_reporting(E_ALL);
-                $start_time = microtime(true);
-                $write_close = false;
-                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                    if(!$write_close) {
-                        $end_time = microtime(true); 
-                        $execution_time = ($end_time - $start_time); 
-                        if($execution_time > 30) {
-                            session_write_close();
-                            $write_close = true;
-                        }
-                    }
-                    if(!$row++) {
-                        $gestionnaireColumn = 0;
-                        $competenceColumn = 1;
-                        $contactColumn = 2;
-                        $fonctionColumn = 3;
-                        $adresseColumn = 4;
-                        $villeColumn = 5;
-                        $emailColumn = 6;
-                        $telephoneColumn = 7;
-                        $departementColumn = 8;
-                        $parcs = $em->getRepository('AppBundle:Gestionnaire')->emptyTable();
-                        continue;
-                    }
-                    // if($row > 10) continue;
-                    $data = array_map("utf8_encode", $data);
-                    $departement = $em->getRepository('AppBundle:Departement')->findOneBy(['code' => $data[$departementColumn]]);
-                    if(empty($departement)) {
-                        continue;
-                    }
-                    $gestionnaire = new Gestionnaire;
-                    $gestionnaire->setGestionnaire($data[$gestionnaireColumn]);
-                    $gestionnaire->setCompetence($data[$competenceColumn]);
-                    $gestionnaire->setContact($data[$contactColumn]);
-                    $gestionnaire->setFonction($data[$fonctionColumn]);
-                    $gestionnaire->setAdresse($data[$adresseColumn]);
-                    $gestionnaire->setVille($data[$villeColumn]);
-                    $gestionnaire->setEmail($data[$emailColumn]);
-                    $gestionnaire->setTelephone($data[$telephoneColumn]);
-                    $gestionnaire->setDepartement($departement);
-                    $em->persist($gestionnaire);
-                    if($row % 50 == 0) {
-                        $em->flush();
-                        echo $row . ',';
-                    }
-                }
-                fclose($handle);
-                $em->flush();
-            }
+            $this->importFile($file_path);
             return new Response('Done');
         } else new Response('Fichier introuvable');
+    }
+
+    private function importFile($file_path)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if (($handle = fopen($file_path, "r")) !== FALSE) {
+            ini_set("memory_limit", "4000M");
+            set_time_limit(3000);
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+            $start_time = microtime(true);
+            $write_close = false;
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                if(!$write_close) {
+                    $end_time = microtime(true); 
+                    $execution_time = ($end_time - $start_time); 
+                    if($execution_time > 30) {
+                        session_write_close();
+                        $write_close = true;
+                    }
+                }
+                if(!$row++) {
+                    $gestionnaireColumn = 0;
+                    $competenceColumn = 1;
+                    $contactColumn = 2;
+                    $fonctionColumn = 3;
+                    $adresseColumn = 4;
+                    $villeColumn = 5;
+                    $emailColumn = 6;
+                    $telephoneColumn = 7;
+                    $departementColumn = 8;
+                    $parcs = $em->getRepository('AppBundle:Gestionnaire')->emptyTable();
+                    continue;
+                }
+                // if($row > 10) continue;
+                $data = array_map("utf8_encode", $data);
+                $departement = $em->getRepository('AppBundle:Departement')->findOneBy(['code' => $data[$departementColumn]]);
+                if(empty($departement)) {
+                    continue;
+                }
+                $gestionnaire = new Gestionnaire;
+                $gestionnaire->setGestionnaire($data[$gestionnaireColumn]);
+                $gestionnaire->setCompetence($data[$competenceColumn]);
+                $gestionnaire->setContact($data[$contactColumn]);
+                $gestionnaire->setFonction($data[$fonctionColumn]);
+                $gestionnaire->setAdresse($data[$adresseColumn]);
+                $gestionnaire->setVille($data[$villeColumn]);
+                $gestionnaire->setEmail($data[$emailColumn]);
+                $gestionnaire->setTelephone($data[$telephoneColumn]);
+                $gestionnaire->setDepartement($departement);
+                $em->persist($gestionnaire);
+                if($row % 50 == 0) {
+                    $em->flush();
+                    echo $row . ',';
+                }
+            }
+            fclose($handle);
+            $em->flush();
+        }
     }
 
     /**
